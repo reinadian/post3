46697ea6c81cc640630dd3e71eae08710d4fca3d
pemrograman web <nabillasaputri.ns@gmail.com>
2025-10-21 05:33:06 -0700

    Initial commit

---
+++ app/src/main/java/com/example/postest3/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.postest3
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    }
