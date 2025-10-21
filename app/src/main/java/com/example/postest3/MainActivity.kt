fd34be06d93070c29a464da28450f69a0e94cf74
pemrograman web <nabillasaputri.ns@gmail.com>
2025-10-21 05:42:45 -0700

    commit

--- app/src/main/java/com/example/postest3/MainActivity.kt
+++ app/src/main/java/com/example/postest3/MainActivity.kt
@@ -7,5 +7,17 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
 
+    private var jenisKelamin = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.apply {
+
+        }
     }
+}
